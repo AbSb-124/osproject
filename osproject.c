39f46065997abbe4a43071c725e3958d2e48ce15
AbSb-124 <49373263+AbSb-124@users.noreply.github.com>
2019-04-18 21:18:46 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -4,7 +4,29 @@
 void main()
 {
 	char p[10][5],temp[5];
-	int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
+	int i,j,pt[10],wt[10],totwt=0,g[10],temp1,n;
 	float avgwt;
-	printf("enter no. of students");
+	printf("Enter number of students:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	printf("enter student %d name:",i+1);
 	scanf("%s",&p[i]);
+	printf("enter process time for each student:");
+	scanf("%d",&pt[i]);
+	printf("enter number of gifts of each student:");
+	scanf("%d",&g[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+          for(j=i+1;j<n;j++)
+	  { 
+		  if(g[i]<g[j])
+			  
+		  {
+	           temp1=g[i];
+		    g[i]=g[j];
+	            
+		  }
+
+	
