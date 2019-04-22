3dcd7e2714b0de0b017c90ab4abe4344ebb7f2b8
AbSb-124 <49373263+AbSb-124@users.noreply.github.com>
2019-04-23 00:13:46 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -1,21 +1,46 @@
-#include<stdio.h>
-#inlcude<conio.h>
-#include<string.h>
-void main()
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+class osproject
 {
-	char p[10][5],temp[5];
-	int i,j,pt[10],wt[10],totwt=0,g[10],temp1,n;
-	float avgwt;
-	printf("Enter number of students:");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-	printf("enter student %d name:",i+1);
-	scanf("%s",&p[i]);
-	printf("enter process time for each student:");
-	scanf("%d",&pt[i]);
-	printf("enter number of gifts of each student:");
-	scanf("%d",&g[i]);
-	}
-	
-	
+  public static void main(String args[])
+  {
+   String[] p = new String[10];
+   String temp;
+   int i;
+   int j;
+   int [] pt = new int [10];
+   int [] pr=new int [10];
+   int temp1;
+   int n;
+   Scanner in = new Scanner(System.in);
+   System.out.println("Enter no of girls");
+   n=in.nextInt();
+   for(i=0;i<n;i++)
+   {
+   	Scanner sc=new Scanner(System.in);
+    System.out.println("Enter girl " + (i+1) + " name:");
+    p[i]=sc.nextLine();
+    System.out.println("Enter process time:");
+    pt[i]=in.nextInt();
+    System.out.println("Enter no of gifts:");
+    pr[i]=in.nextInt();
+   }
+   for(i=0;i<n-1;i++)
+   {
+    for(j=i+1;j<n;j++)
+    {
+      if(pr[i]<pr[j])
+    {
+      temp1=pr[i];
+      pr[i]=pr[j];
+      pr[j]=temp1;
+      temp1=pt[i];
+      pt[i]=pt[j];
+      pt[j]=temp1;
+      
+      temp = p[i];
+      p[i] = p[j];
+      p[j] = temp;
+ }
+ }
