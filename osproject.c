0320c6a6f1fda8e247d383b822732a03521f20a5
AbSb-124 <49373263+AbSb-124@users.noreply.github.com>
2019-04-24 22:48:53 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -14,15 +14,42 @@ class osproject
    int temp1;
    int n;
    Scanner in = new Scanner(System.in);
-   System.out.println("Enter Number Of Students");
+   System.out.println("Enter no of students");
    n=in.nextInt();
    for(i=0;i<n;i++)
    {
    	Scanner sc=new Scanner(System.in);
-    System.out.println("Enter Student" + (i+1) + " Name:");
+    System.out.println("Enter student " + (i+1) + " name:");
     p[i]=sc.nextLine();
-    System.out.println("Enter Process Time:");
+    System.out.println("Enter process time:");
     pt[i]=in.nextInt();
-    System.out.println("Enter Number Of Gifts:");
+    System.out.println("Enter no of gifts:");
     pr[i]=in.nextInt();
    }
+   for(i=0;i<n-1;i++)
+   {
+    for(j=i+1;j<n;j++)
+    {
+      if(pr[i]<pr[j])
+    {
+      temp1=pr[i];
+      pr[i]=pr[j];
+      pr[j]=temp1;
+      temp1=pt[i];
+      pt[i]=pt[j];
+      pt[j]=temp1;
+      
+      temp = p[i];
+      p[i] = p[j];
+      p[j] = temp;
+    } 
+   
+   }
+   }
+   System.out.println("S_name   P_time    No.Gifts");
+   for(i=0;i<n;i++)
+   {
+   	System.out.println( " "+p[i]+"       "+pt[i]+"           "+pr[i] );
+   }
+   }
+}
